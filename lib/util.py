2f7cd94324aa4fc29825f710d2271cd9f5f964f4
JB Ladera <domobaby10@gmail.com>
2022-02-19 10:03:39 -0600

    added pylintrc

--- lib/util.py
+++ lib/util.py
@@ -4,7 +4,7 @@ from . import constants as _Constants
 
 def pack(command: int, seq: int, session_id: int, data: str = None) -> bytes:
     header = _struct.pack("!HBBII", _Constants.MAGIC_NUMBER, _Constants.VERSION, command, seq, session_id)
-    if data == None:
+    if data is None:
         return header
     return header + data.encode("utf-8", "replace")
 
