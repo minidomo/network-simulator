781b3eb6e856eef8b9088cae64ebb9e5a251b6f6
John Ladera <jbladera@cs.utexas.edu>
2022-02-24 18:02:13 -0600

    finished client thread and event based

--- lib/structures/client_data.py
+++ lib/structures/client_data.py
@@ -1,21 +1,15 @@
-from ..constants import Command
+from ..constants import Command as _Command
 
 
 class ClientData:
-    session_id: int
-    connected: bool
-    packet_number: int
-    address: "tuple[str,int]"
-    timestamp: float
-    previous_command: int
 
     def __init__(self, session_id: int, address: "tuple[str,int]"):
-        self.session_id = session_id
-        self.connected = True
-        self.packet_number = 0
-        self.address = address
-        self.timestamp = -1
-        self.previous_command = Command.DEFAULT.value
+        self.session_id: int = session_id
+        self.connected: bool = True
+        self.packet_number: int = 0
+        self.address: "tuple[str,int]" = address
+        self.timestamp: int = -1
+        self.previous_command: int = _Command.DEFAULT.value
 
     def disconnect(self):
         self.connected = False
